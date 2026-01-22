f50b1345fb7a5376ed4a4f81974cf58a5ced42b0
Arthur <arthur.edlp@outlook.fr>
2026-01-22 11:42:14 +0100

    garde uniquement les variables nécessaire

--- code.R
+++ code.R
@@ -1,1 +1,9 @@
+
+
 data <- read.table("LOCKHEEDMARTIN.txt", header = TRUE, sep = "", stringsAsFactors = FALSE)
+
+summary(data)
+
+str(data)
+
+data[c("ouv", "haut", "bas", "vol", "devise")] <- NULL
